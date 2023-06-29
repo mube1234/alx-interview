d1d20de57447914077f1a876360cede3575dd923
Mubarik <burkatech2021@gmail.com>
2023-06-29 18:16:32 +0000

    hot-fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-"""Pascal"""
+"""Pascal's Triangle"""
+
 
 def pascal_triangle(n):
-    """function to get the number of pascal triangle"""
+    """
+    a function that returns a list
+    of integers representing the
+    pascal triangle of n:
+       . Returns an empty list if n <= 0
+       . assume n will be always an integer
+    """
+    pascal_tri = []
+
     if n <= 0:
         return []
 
-    pascal_triangle = [0] * n
-
     for i in range(n):
-        new_row = [0] * (i+1)
-        new_row[0] = 1
-        new_row[len(new_row) - 1] = 1
+        if (i == 0):
+            pascal_tri.append([1])
+        else:
+            cur_row = []
+            for j in range(i + 1):
+                if (j == 0 or j == i):
+                    cur_row.append(1)
+                else:
+                    cur_row.append(pascal_tri[i - 1][j - 1] +
+                                   pascal_tri[i - 1][j])
 
-        for j in range(1, i):
-            if j > 0 and j < len(new_row):
-                a = pascal_triangle[i - 1][j]
-                b = pascal_triangle[i - 1][j - 1]
-                new_row[j] = a + b
+            pascal_tri.append(cur_row)
 
-        pascal_triangle[i] = new_row
-
-    return pascal_triangle
+    return (pascal_tri)
